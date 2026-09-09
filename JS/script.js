4ada46d25a1677a0de575bd130c04a54f989b8bf
HENRY GABRIEL DOS SANTOS BARBOSA <henry.gsbarbosa@senacsp.edu.br>
2026-09-08 21:52:35 -0300

    Exercicios 2 e 3

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,34 @@ function verificar() {
         resultado.innerHTML = "Aprovado"
     }
 }
+
+let numero1, numero2;
+
+function subtrair(){
+    numero1 = Number(document.getElementById('numero1').value);
+    numero2 = Number(document.getElementById('numero2').value);
+
+    resultado1 = numero1 - numero2
+
+    document.getElementById('resultado1').innerHTML ="Resultado: " + resultado1;
+}
+
+let N1,N2,N3,N4, aprovacao;
+
+function media(){
+    N1 = Number(document.getElementById('N1').value);
+    N2 = Number(document.getElementById('N2').value);
+    N3 = Number(document.getElementById('N3').value);
+    N4 = Number(document.getElementById('N4').value);
+    aprovacao =document.getElementById('aprovacao');
+
+    resultado2 = (N1+N2+N3+N4)/4
+
+    document.getElementById('resultado2').innerHTML= "A média é: " + resultado2
+
+    if (resultado2 >=5){
+        aprovacao.innerHTML="Aprovado"
+    }else{
+        aprovacao.innerHTML="Reprovado"
+    }
+}
